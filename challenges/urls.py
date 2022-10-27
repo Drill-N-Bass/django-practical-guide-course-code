cbd8729ea79c7c7cc6ecd962e39d6f087a03a1fb
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-10-27 15:22:14 +0200

    documentation

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index), # /challenges/
-    path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge, name="month-challenge")
+    path("<int:month>", views.monthly_challenge_by_number), # filter usreinput `<int:month>` just int
+    path("<str:month>", views.monthly_challenge, name="month-challenge") # filter usreinput `<str:month>` just str
 ]
